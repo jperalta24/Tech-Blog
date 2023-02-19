240b57a30d65edb97508b69c6aa9a677d4744d26
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-02-18 18:29:33 -0800

    homeRoutes

---
+++ controllers/dashBoardRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+
+//All user posts ('/dashboard')
+
+
+// Get one post to edit ('dashboard/edit/:id')
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,65 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ["content"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
 
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      // username: req.session.username,
+      // userId: req.session.userId,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-
-
-
-
+// get a single post ('/post/:id')
+router.get("/posts/:id", async (req, res) => {
+  try{
+  const postData = await Post.findByPk(req.params.id, {
+    include: [
+      {
+        model: Comment,
+        attributes: ['content'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username']
+      },
+    ]
+  });
+  if (postData){
+  const post = postData.map((post) => post.get({ plain: true}));
+  console.log(post);
+  res.render('singlePost', {
+    post, loggedIn: req.session.loggedIn, username: req.session.username,
+  })
+} else {
+  res.status(404).json({message: "This id does not have a post"})
+}
+} catch
+  (err) {
+    res.status(500).json(err);
+};
+});
 module.exports = router;
