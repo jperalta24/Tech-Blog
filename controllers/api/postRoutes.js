0ac4dccf76ae51172144c805438e13af42e99a52
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-02-22 18:42:25 -0800

    post routes edits

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,41 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
+// create a new post
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+// update a post
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!postData) {
+      res.status(404).json({ message: "This id is has not post" });
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
