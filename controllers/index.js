cf26f92c87fea9f3508bbb1bd0e3b85c84e2f294
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-04-15 22:59:10 -0700

    new commit

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const apiRoutes = require('./api')
 const homeRoutes = require("./homeRoutes");
-const dashboardRoutes = require('./dashboardRoutes');
+const dashboardRoutes = require('./dashBoardRoutes');
 
 router.use('/api', apiRoutes);
 router.use("/", homeRoutes);
